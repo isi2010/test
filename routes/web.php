a1dddfc13cbb21bd27e1461294f16cf594fd3ac8
isi2010 <108588893+isi2010@users.noreply.github.com>
2022-08-01 14:47:49 +0200

    New file

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+/*
+ * These routes use the root namespace 'App\Http\Controllers\Web'.
+ */
+Route::namespace('Web')->group(function () {
+
+    // All the folder based web routes
+    include_route_files('web');
+
+    // Website home route
+    Route::get('/', 'HomeController@index')->name('home');
+});
